0c20f7d7c839babf896c13e872d5a800dcbfa801
Verin1005 <daqingchong0809@gmail.com>
2022-09-06 22:29:16 +0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.6.0",
+            },
+        ],
+    },
+};
